f92a1fd86ba1d5507322f702b50fea98da4ecc55
Martin DAVILA <aldntmi@gmail.com>
2024-09-16 10:03:52 +0200

    Vercel congif to use node 20

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_Br-3et6s.mjs';
-import { manifest } from './manifest_DDRlhxjX.mjs';
+import { manifest } from './manifest_BUCHa2nf.mjs';
 import { onRequest } from './_noop-middleware.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -43,7 +43,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "92843ee6-391c-4a9e-945f-c5428698c1cf",
+    "middlewareSecret": "f1bff8e5-6738-4f46-b650-1572c2fc5331",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
